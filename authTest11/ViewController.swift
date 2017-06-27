5109ab616a23f20c9613bdb94e194f3f854ba959
user <user@imac-user.local>
2017-06-27 19:01:58 +0500

    rrrrrrrrrr

--- authTest11/ViewController.swift
+++ authTest11/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("efwefwef")
+        print("1231231231231")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
