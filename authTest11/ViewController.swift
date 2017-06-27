fc6398d5bbf55885acee9ce7a8c6f2ffbdc74281
johhhny <admin@imacjohhhny.local>
2017-06-27 19:10:55 +0500

    qqqqqqqqqq

--- authTest11/ViewController.swift
+++ authTest11/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("efwefwef")
         print("1231231231231")
+        print("1223123131wtwegwegwegw")
+        print("efwefwef")
+        print("1231231231231")
+        print("1223123131wtwegwegwegw")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
